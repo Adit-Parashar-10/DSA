5e40fa6ebec299356a0b0a07114239792356678c
Adit Parashar <parasharadit86@gmail.com>
2024-12-25 22:36:19 +0530

    added Q2

---
+++ 02.Second_Largest_Element_in_an_Array_without_sorting.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int getSecondLargest(int arr[], int n) {
+    int lg = INT16_MIN;
+    int slg = INT16_MIN;
+    for(int i=0; i<n; i++){
+            
+        if(arr[i]>lg){
+            slg = lg;
+            lg = arr[i];
+        }
+        else if(arr[i]<lg && arr[i]>slg){
+            slg = arr[i];
+        }
+    }
+    return (slg == INT16_MIN) ? -1 : slg;
+}  
+
+int main(){
+    int n;
+    cout<<"Enter the size of an array:";
+    cin>>n;
+    int arr[n];
+    cout<<"enter the value of elements:";
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    int ans = getSecondLargest(arr,n);
+
+    cout<<"second largest array is:"<<endl;
+
+    return 0;
+}
