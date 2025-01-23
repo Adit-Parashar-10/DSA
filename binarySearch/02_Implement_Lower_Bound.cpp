da26800e9d9e4e4828d36adf020fdc5fa1ac48eb
Adit Parashar <parasharadit86@gmail.com>
2025-01-23 22:18:23 +0530

    updated code

--- binarySearch/02_Implement_Lower_Bound.cpp
+++ binarySearch/02_Implement_Lower_Bound.cpp
@@ -1,20 +1,46 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+
+int lowerBound(int arr[], int s, int e, int k)
+{
+    int res = -1;
+    int mid = s + (e - s) / 2;
+    while (s <= e)
+    {
+        if (arr[mid] <= k)
+        {
+            res = mid;
+            s = mid + 1;
+        }
+        else
+        {
+            e = mid - 1;
+        }
+    }
+    return res;
+}
+
+int main()
+{
     int n;
-    cout<<"Enter the size of an array:";
-    cin>>n;
+    cout << "Enter the size of an array:";
+    cin >> n;
     int arr[n];
-    cout<<"enter the value of elements:";
-    for(int i=0; i<n; i++){
-        cin>>arr[i];
+    cout << "enter the value of elements:";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
 
     int k;
-    cout<<"enter the value for lower bound";
-    cin>>k;
+    cout << "enter the value for lower bound:";
+    cin >> k;
 
-    
+    int s = 0, e = n - 1;
+
+    int ans = lowerBound(arr, s, e, k);
+
+    cout << "lower bound for element " << k << " is found at index:" << ans << endl;
 
     return 0;
 }
