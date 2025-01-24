dd335579680f55fae503646333cc58c73f21b915
Adit Parashar <parasharadit86@gmail.com>
2025-01-24 23:39:45 +0530

    updated code

--- LinkedList/LLcreation.cpp
+++ LinkedList/LLcreation.cpp
@@ -1,15 +1,39 @@
 #include<iostream>
 using namespace std;
-int main(){
-    int n;
-    cout<<"Enter the size of an array:";
-    cin>>n;
-    int arr[n];
-    cout<<"enter the value of elements:";
-    for(int i=0; i<n; i++){
-        cin>>arr[i];
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(){
+        this->data = 0;
+        this->next=NULL;
     }
 
+    Node(int data){
+        this->data = data;
+        this->next=NULL;
+    }
+
+};
+int main(){
+    
+    Node* head = new Node(1);
+    Node* second = new Node(2);
+    Node* third = new Node(3);
+
+    head->next = second;
+    second->next = third;
+    third->next = NULL;
+
+    Node* temp = head;
+
+    while(temp!= NULL){
+        cout<<temp->data<<"-->";
+        temp = temp->next;
+    }
+    cout<<"NULL";
 
     return 0;
 }
